f86896f90c4655d0ebb1295df9fead048f9edfe3
ustin <rykova.tanya.96@gmail.com>
2023-10-01 21:50:59 +0300

    Add ZOO

---
+++ задание/Animals.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace AnimalsTask
+{
+    public class Animals
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Species { get; set; }
+        public string Biome { get; set; }
+        public double SquareArea { get; set; }
+        public string[] Diet { get; set; }
+        public string Sound { get; set; }
+
+        public void Eat()
+        {
+            Console.WriteLine($"Животное {Name} ест");
+        }
+
+        public void MakeSound()
+        {
+            Console.WriteLine($"Животное {Name} издает звук: {Sound}");
+        }
+
+        public void Play()
+        {
+            Console.WriteLine($"Животное {Name} играет");
+        }
+    }
+}
---
+++ задание/Program.cs
@@ -0,0 +1,15 @@
+﻿using AnimalsTask;
+Animals elephantPetya = new Animals();
+Animals penguinSimba = new Animals();
+Animals tigerMatilda = new Animals();
+
+
+
+elephantPetya.Name = "Петя";
+elephantPetya.Age = 5;
+elephantPetya.Species = "Слон";
+elephantPetya.Biome = "Тропики";
+elephantPetya.SquareArea = 10;
+elephantPetya.Diet = new string[] { "Сено" };
+elephantPetya.Sound = "Рррр";
+
